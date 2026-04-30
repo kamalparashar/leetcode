ea0f267492de5321ace496bad9b1e2d2d90db9c8
Kamal Parashar <81461545+kamalparashar@users.noreply.github.com>
2026-04-30 07:34:57 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 97.45%] - LeetPush

---
+++ Sort-Colors.cpp
@@ -0,0 +1,21 @@
+1class Solution {
+2public:
+3    void sortColors(vector<int>& nums) {
+4        int low = 0, mid = 0, high = nums.size()-1;
+5        while(mid<=high){
+6            if(nums[mid] == 0){
+7                swap(nums[low],nums[mid]);
+8                low++;
+9                mid++;
+10            }
+11            else if(nums[mid] == 2){
+12                swap(nums[mid],nums[high]);
+13                high--;
+14            }
+15            else{
+16                mid++;
+17            }
+18            // mid++;
+19        }
+20    }
+21};
