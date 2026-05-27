cff63b62e139e7affb8678005b6e485fb63dad9b
Kamal Parashar <81461545+kamalparashar@users.noreply.github.com>
2026-05-27 09:23:26 +0530

    [239] [Time Beats: 44.12%] [Memory Beats: 24.89%] - LeetPush

--- Sliding-Window-Maximum.cpp
+++ Sliding-Window-Maximum.cpp
@@ -1,28 +1,30 @@
 1class Solution {
 2public:
 3    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-4        vector<int> ans;
-5        deque<int> q;
-6        int n = nums.size();
-7
-8        for(int i=0;i<k;i++){
-9            while(!q.empty() && q.back() < nums[i]){
-10                q.pop_back();
-11            }
-12            q.push_back(nums[i]);
-13        }
-14        ans.push_back(q.front());
-15
-16        for(int i=k;i<n;i++){
-17            if(!q.empty() && q.front()==nums[i-k]){
-18                q.pop_front();
-19            }
-20            while(!q.empty() && q.back() < nums[i]){
-21                q.pop_back();
-22            }
-23            q.push_back(nums[i]);
-24            ans.push_back(q.front());
-25        }
-26        return ans;
-27    }
-28};
+4        // read cp handbook sliding window
+5        
+6        vector<int> ans;
+7        deque<int> q;
+8        int n = nums.size();
+9
+10        for(int i=0;i<k;i++){
+11            while(!q.empty() && q.back() < nums[i]){
+12                q.pop_back();
+13            }
+14            q.push_back(nums[i]);
+15        }
+16        ans.push_back(q.front());
+17
+18        for(int i=k;i<n;i++){
+19            if(!q.empty() && q.front()==nums[i-k]){
+20                q.pop_front();
+21            }
+22            while(!q.empty() && q.back() < nums[i]){
+23                q.pop_back();
+24            }
+25            q.push_back(nums[i]);
+26            ans.push_back(q.front());
+27        }
+28        return ans;
+29    }
+30};
