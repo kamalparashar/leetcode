eb3f5969660ff15f5c71af05e65854784c190bcc
Kamal Parashar <81461545+kamalparashar@users.noreply.github.com>
2026-05-28 09:41:42 +0530

    [567] [Time Beats: 100.00%] [Memory Beats: 70.11%] - LeetPush

--- Permutation-in-String.cpp
+++ Permutation-in-String.cpp
@@ -2,26 +2,35 @@
 2public:
 3    bool checkInclusion(string s1, string s2) {
 4        int n = s1.length(), m = s2.length();
-5        sort(s1.begin(),s1.end());
-6
-7        map<char,int> mp;
-8
-9        for(auto &s:s1){
-10            mp[s]++;
-11        }
-12
-13        for(int i=0;i<m-n+1;i++){
-14            string s = s2.substr(i,n);
-15            map<char,int> mp2;
-16
-17            for(auto &j:s){
-18                mp2[j]++;
+5        vector<int> freq1(26,0);
+6        vector<int> freq2(26,0);
+7        
+8        for(auto &s:s1){
+9            freq1[s-'a']++;
+10        }
+11
+12        int i=0, j=0;
+13        while(i<m && j<m){
+14            freq2[s2[j]-'a']++;
+15
+16            while(j-i+1 > n){
+17                freq2[s2[i]-'a']--;
+18                i++;
 19            }
-20            
-21            if(mp == mp2){
-22                return true;
-23            }
-24        }
-25        return false;
-26    }
-27};
+20            if(j-i+1 == n){
+21                bool b = true;
+22                for(int k=0;k<26;k++){
+23                    if(freq1[k]!=freq2[k]){
+24                        b = false;
+25                        break;
+26                    }
+27                }
+28                if(b){
+29                    return true;
+30                }
+31            }
+32            j++;
+33        }
+34        return false;
+35    }
+36};
