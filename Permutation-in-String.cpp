eb008b20688b6659541ff2f50aa65f105a6a548f
Kamal Parashar <81461545+kamalparashar@users.noreply.github.com>
2026-05-28 09:29:18 +0530

    [567] [Time Beats: 5.05%] [Memory Beats: 5.00%] - LeetPush

--- Permutation-in-String.cpp
+++ Permutation-in-String.cpp
@@ -4,13 +4,24 @@
 4        int n = s1.length(), m = s2.length();
 5        sort(s1.begin(),s1.end());
 6
-7        for(int i=0;i<m-n+1;i++){
-8            string s = s2.substr(i,n);
-9            sort(s.begin(),s.end());
-10            if(s1 == s){
-11                return true;
-12            }
-13        }
-14        return false;
-15    }
-16};
+7        map<char,int> mp;
+8
+9        for(auto &s:s1){
+10            mp[s]++;
+11        }
+12
+13        for(int i=0;i<m-n+1;i++){
+14            string s = s2.substr(i,n);
+15            map<char,int> mp2;
+16
+17            for(auto &j:s){
+18                mp2[j]++;
+19            }
+20            
+21            if(mp == mp2){
+22                return true;
+23            }
+24        }
+25        return false;
+26    }
+27};
