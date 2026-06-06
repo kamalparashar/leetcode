22256930cfa1c6719ca0c296d38a8db53267fbe4
Kamal Parashar <81461545+kamalparashar@users.noreply.github.com>
2026-06-06 18:56:09 +0530

    [3] [Time Beats: 68.85%] [Memory Beats: 40.47%] - LeetPush

--- Longest-Substring-Without-Repeating-Characters.cpp
+++ Longest-Substring-Without-Repeating-Characters.cpp
@@ -5,19 +5,19 @@
 5            return s.length(); 
 6        }
 7        int i=0,j=0, n=s.length(), ans = INT_MIN, count = 0;
-8        unordered_map<char, pair<int,int>> v;
+8        unordered_map<char, int> v;    //char,pos
 9        while(j<n){
 10            if(v.find(s[j]) != v.end()){
 11                count = j-i;
-12                for(int k=i;k<v[s[j]].first;k++){
-13         v.erase(s[k]);
+12                for(int k=i;k<v[s[j]];k++){
+13                    v.erase(s[k]);
 14                }
-15                i=v[s[j]].first+1;
-16                v[s[j]].first = j;
+15                i=v[s[j]]+1;
+16                v[s[j]] = j;
 17                ans = max(ans, count);
 18            }
 19            else{
-20                v.insert({s[j], {j,1}});
+20                v.insert({s[j], j});
 21            }            
 22            j++;
 23        }
