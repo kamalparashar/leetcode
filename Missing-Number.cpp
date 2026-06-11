a374baf5052a1c4996b37aeeb89dec3d628ff520
Kamal Parashar <81461545+kamalparashar@users.noreply.github.com>
2026-06-11 18:04:25 +0530

    [268] [Time Beats: 100.00%] [Memory Beats: 37.37%] - LeetPush

--- Missing-Number.cpp
+++ Missing-Number.cpp
@@ -1,17 +1,11 @@
 1class Solution {
 2public:
 3    int missingNumber(vector<int>& nums) {
-4        int n = nums.size();
-5        vector<bool> v(n+1,0);
-6        for(int i:nums){
-7            v[i] = 1;
-8        }
-9        for(int i=0;i<=n;i++){
-10            if(!v[i]){
-11                return i;
-12            }
-13        }
-14        return -1;
-15        
-16    }
-17};
+4        int n = nums.size(), num = 0;
+5        for(int i=0;i<n;i++){
+6            num ^= nums[i]^(i+1);
+7        }
+8        return num;
+9        
+10    }
+11};
